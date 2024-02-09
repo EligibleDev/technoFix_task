829a74e990dc6405046b2574068a61012d84977b
EligibleDev <eligibledev@gmail.com>
2024-02-10 00:38:16 +0600

    basic setup done

---
+++ src/routers/MainRouter/MainRouter.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../../layouts/MainLayout/MainLayout";
+import Home from "../../pages/Home/Home";
+
+const MainRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+        ],
+    },
+]);
+
+export default MainRouter;
