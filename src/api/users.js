1f72f3e5ad7d98fc04eb7f97a88229eef9987119
EligibleDev <eligibledev@gmail.com>
2024-02-10 18:39:10 +0600

    toast setup done

--- src/api/users.js
+++ src/api/users.js
@@ -9,3 +9,20 @@ export const getUserDetails = async (id) => {
       const { data } = await myAxios.get(`/users/${id}`)
       return data;
 }
+
+export const addUser = async (user) => {
+      const { data } = await myAxios.post('/users/add', user);
+      return data;
+}
+
+export const uploadImage = async (image) => {
+      const formData = new FormData();
+      formData.append("image", image);
+
+      const { data } = await myAxios.post(
+            `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_imgBbApiKey}`,
+            formData
+      );
+
+      return data;
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { getAllUsers } from "../../api/users";
+import { addUser, getAllUsers, uploadImage } from "../../api/users";
 import UserCard from "../../components/UserCard/UserCard";
-import { Input, Spinner } from "@material-tailwind/react";
+import { Button, Input } from "@material-tailwind/react";
 import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
+import Filters from "../../components/Filters/Filters";
+import toast from "react-hot-toast";
 
 const Home = () => {
     const [users, setUsers] = useState([]);
@@ -13,6 +15,76 @@ const Home = () => {
     const handleSearch = async (e) => {
         const value = e.target.value;
         setQuery(value);
+    };
+
+    const handleSort = async (e) => {
+        const method = e.target.value;
+
+        if (method === "name") {
+            const sortedUsers = [...users].sort(function (a, b) {
+                var nameA = a.firstName.trim().toUpperCase();
+                var nameB = b.firstName.trim().toUpperCase();
+                return nameA.localeCompare(nameB);
+            });
+            setUsers(sortedUsers);
+        } else if (method === "email") {
+            const sortedUsers = [...users].sort(function (a, b) {
+                var emailA = a.email.trim().toUpperCase();
+                var emailB = b.email.trim().toUpperCase();
+                return emailA.localeCompare(emailB);
+            });
+            setUsers(sortedUsers);
+        } else if (method === "company") {
+            const sortedUsers = [...users].sort(function (a, b) {
+                var companyA = a.company.name.trim().toUpperCase();
+                var companyB = b.company.name.trim().toUpperCase();
+                return companyA.localeCompare(companyB);
+            });
+            setUsers(sortedUsers);
+        } else {
+            setLoading(true);
+            fetchAllUsers(query);
+            setLoading(false);
+        }
+    };
+
+    const handleAddUser = async (e) => {
+        e.preventDefault();
+        const toastId = toast.loading("Adding User...");
+
+        const firstName = e.target.firstName.value;
+        const lastName = e.target.lastName.value;
+        const position = e.target.position.value;
+        const companyName = e.target.company.value;
+        const email = e.target.email.value;
+        const location = e.target.address.value;
+        const city = e.target.city.value;
+        const image = e.target.image.files[0];
+
+        try {
+            const imageData = await uploadImage(image);
+            const newUser = {
+                firstName,
+                lastName,
+                company: {
+                    name: companyName,
+                    title: position,
+                },
+                email,
+                address: {
+                    address: location,
+                    city,
+                },
+                image: imageData?.data?.url,
+            };
+
+            const res = await addUser(newUser);
+            toast.success("User Added", { id: toastId });
+            e.target.reset();
+        } catch (error) {
+            console.error(error);
+            toast.error(error.message, { id: toastId });
+        }
     };
 
     const fetchAllUsers = async (query) => {
@@ -29,26 +101,52 @@ const Home = () => {
         setLoading(true);
         fetchAllUsers(query);
         setLoading(false);
+        console.log();
     }, [query]);
 
     return (
         <>
-            <>
-                <section className="container mx-auto flex justify-between">
-                    <aside className="w-1/4">
-                        <Input onChange={handleSearch} label="search..." />
-                    </aside>
+            <section className="container mx-auto flex justify-between gap-4">
+                <aside className="w-1/4">
+                    <form
+                        className="flex flex-col justify-center gap-3 sticky top-4"
+                        onSubmit={handleAddUser}
+                    >
+                        <Input required type="text" name="firstName" label="First Name" />
+                        <Input required type="text" name="lastName" label="Last Name" />
+                        <Input
+                            required
+                            type="text"
+                            name="position"
+                            label="Job Position"
+                        />
+                        <Input
+                            required
+                            type="text"
+                            name="company"
+                            label="Company's Name"
+                        />
+                        <Input required type="text" name="email" label="Email Address" />
+                        <Input required type="text" name="address" label="Address" />
+                        <Input required type="text" name="city" label="City" />
+                        <input name="image" required type="file" />
+                        <Button type="submit">Add User</Button>
+                    </form>
+                </aside>
+
+                <div className="w-3/4 space-y-4">
+                    <Filters handleSort={handleSort} handleSearch={handleSearch} />{" "}
                     {loading ? (
                         <LoadingSpinner />
                     ) : (
-                        <div className="w-3/4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
                             {users?.map((user) => (
                                 <UserCard user={user} key={user?.id} />
                             ))}
                         </div>
                     )}
-                </section>
-            </>
+                </div>
+            </section>
         </>
     );
 };
