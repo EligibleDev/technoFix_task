70a96b7c05114a94c6ec913e8a789e6761667e8a
EligibleDev <eligibledev@gmail.com>
2024-02-10 19:22:24 +0600

    header done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-      return (
-            <div>
-                  header
-            </div>
-      );
+    return (
+        <header className="text-5xl font-bold text-center py-4 mb-4 border-b border-b-gray-500">
+            <Link to="/">DummyUsers</Link>
+        </header>
+    );
 };
 
 export default Header;
